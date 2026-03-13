09c869d7bfc897b044fb3829d81973b293dd17b4
TomChienXu <truongss04@gmail.com>
2026-03-13 17:21:49 +0700

    paging

--- src/api/useApi.tsx
+++ src/api/useApi.tsx
@@ -1,6 +1,13 @@
+import { Query } from "@tanstack/react-query"
 import axios from "axios"
 
-export const getUsers = async () => {
-    const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+export const getUsers = async ({queryKey}) => {
+    const [_key, page, limit] = queryKey;
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts", {
+        params: {
+            page,
+            limit
+        }
+    })
     return response.data
 }
--- src/pages/userPage.tsx
+++ src/pages/userPage.tsx
@@ -1,9 +1,23 @@
 import { Button, Table } from "antd";
 import { useUsers } from "../hooks/useUser"
+import { useState } from "react";
 
 
 export const UserPage = () => {
     const { data, isError, isLoading, error, refetch, isFetching } = useUsers();
+    const [tableParams, setTableParams] = useState({
+        current: 1,
+        pageSize: 10,
+    })
+
+
+    const handleTableChange = (pagination) => {
+        setTableParams({
+            current: pagination.current,
+            pageSize: pagination.pageSize,
+        })
+
+    }
 
     if (isLoading) {
         return <h3 style={{ textAlign: "center", marginTop: "20px" }}>Đang tải dữ liệu bài viết...</h3>;
@@ -35,7 +49,12 @@ export const UserPage = () => {
     return (
         <>
             <Button onClick={() => refetch()} loading={isFetching} type="primary" >Refetch</Button>
-            <Table columns={columns} dataSource={data} />
+            <Table pagination={{
+                current: tableParams.current,
+                pageSize: tableParams.pageSize,
+                showSizeChanger: true,
+            }} 
+            onChange={handleTableChange} columns={columns} dataSource={data} />
         </>
     )
 }
